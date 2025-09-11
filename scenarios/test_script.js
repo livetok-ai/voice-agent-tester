d823c2062ef96fc047eeed0678afe1f5978c3434
Gustavo Garcia <gustavogb@gmail.com>
2025-09-11 20:46:22 +0200

    Initial

---
+++ scenarios/test_script.js
@@ -0,0 +1,3 @@
+// This is a placeholder for your JavaScript injection.
+// You can add functions here to interact with the voice agent.
+console.log("test_script.js loaded.");
---
+++ voice_agent_tester/__report.py
@@ -0,0 +1,6 @@
+
+# unexpected responses
+# average response time
+# robustness to audio distortion
+# handling wrong scenarios (tell me a joke)
+# tool calling
